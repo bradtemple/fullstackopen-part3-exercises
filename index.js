45781e972e1fefa63b92d95bc44488ef40af99cf
Brad Temple <brad.temple@gmail.com>
2020-05-28 14:48:17 -0700

    heroku

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('body', req => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
@@ -102,7 +104,7 @@ const generateId = () => {
   res.json(person)  
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
